4043a34072a18d174fbabcc8c35c39716f7c5558
zhanghong <hong.zhang@axinan.com>
2025-02-27 16:11:38 +0800

    chore: update template

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -5,14 +5,34 @@
  */
 import { CURRENCY } from "@/constants/currency";
 
+type PriceOption = {
+  withCurrency?: boolean;
+  decimal?: number;
+  defaultZeroDisplay?: string;
+  defaultEmptyDisplay?: string;
+};
+
 export const formatPrice = (
   price: string | number | undefined | null,
-  withCurrency?: boolean,
+  options?: PriceOption,
 ): string => {
-  if (price === null || price === undefined) return '-';
+  const {
+    withCurrency = true,
+    decimal = 0,
+    defaultZeroDisplay = '-',
+    defaultEmptyDisplay = '-',
+  } = options ?? {};
+  if (price === null || price === undefined) return defaultEmptyDisplay;
+  if (Number(price) === 0) return defaultZeroDisplay;
   if (typeof price === 'number' && isNaN(price)) return '-';
   const numParts = price.toString().split('.');
-  numParts[0] = numParts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  if (decimal > 0) {
+    numParts[1] = numParts[1] || '';
+    numParts[1] = numParts[1].padEnd(decimal, '0');
+  } else {
+    numParts.splice(1, 1);
+  }
+  numParts[0] = numParts[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.');
   const number = numParts.join('.');
-  return `${number}${withCurrency ? CURRENCY : ''}`;
+  return `${withCurrency ? CURRENCY : ''}${number}`;
 };
